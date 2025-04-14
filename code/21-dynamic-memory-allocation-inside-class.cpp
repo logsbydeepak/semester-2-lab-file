bcf630aa4ad6c2360a8365d018cdc92aa2deed1b
deepak <96181928+logsbydeepak@users.noreply.github.com>
2025-04-14 15:56:35 +0530

    remove comments

--- code/21-dynamic-memory-allocation-inside-class.cpp
+++ code/21-dynamic-memory-allocation-inside-class.cpp
@@ -3,19 +3,16 @@ using namespace std;
 
 class Number {
 private:
-  int *num; // pointer to dynamically allocated int
+  int *num;
 
 public:
-  // Constructor
   Number(int value) {
     num = new int; // allocate memory
     *num = value;  // set value
   }
 
-  // Function to display the number
   void show() { cout << "Value: " << *num << endl; }
 
-  // Destructor
   ~Number() {
     delete num; // free memory
     cout << "Memory deallocated" << endl;
@@ -26,8 +23,8 @@ int main() {
   Number n(42); // create object with dynamic int
   n.show();
 
-  Number *ptr = new Number(100); // Create object using pointer
-  ptr->show();                   // Access member function using pointer
+  Number *ptr = new Number(100); // create object using pointer
+  ptr->show();                   // access member function using pointer
 
   delete ptr; // Free memory
 
